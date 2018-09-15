ca5e82d597cdda452f96ae7db499ad632f39f137
jodnddus <lockhost23@gmail.com>
2018-09-15 23:48:20 +0900

    npm install discord-youtube-api

--- RobotBot/App.js
+++ RobotBot/App.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require('./config.json');
+const Youtube = require('discord-youtube-api');
+const youtube = new Youtube("")
 
 bot.on("ready", () => {
   console.log("Ro is comeback!!!")
